ac615fe714cedabb8136582b5546f1674480827d
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2023-12-14 10:18:36 +1300

    Update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Email API - Cloudflare Worker 📨
+# Email API - Cloudflare Worker
 
 This email api is a Cloudflare Worker that allows you to send emails from your app using Cloudflare Workers and its partener provider MailChannel.net (Free email transations via Cloudflare workers).
 
@@ -9,23 +9,49 @@ This email api is a Cloudflare Worker that allows you to send emails from your a
 
 ## Usage Example
 
+Update your DNS TXT record to point to the MailChannel.net server. (This is a one time setup)
+`v=mc1 cfid=<<WORKER_SUBDOMAIN>>.workers.dev cfid=<<DOMAIN_SEND_FROM>>`
+
+Use PostMan, ThunderClient or CURL to send a POST request to the Cloudflare Worker URL with the following headers and body.
+
+- Worker URL: <<CF_WORKER_URL_HERE>>
+- Authorization: Bearer <<EMAIL_TOKEN>>
+- Content-Type: application/json
+
+- JSON Body:
+```json
+{
+  "to": [{
+      "name": "To User Name",
+      "email": "to@chrisbrocklesby.com"
+  }],
+  "from": {
+    "name": "From User Name",
+    "email": "from@chrisbrocklesby.com"
+  },
+  "subject": "Email Subject",
+  "html": "<h1>HTML</h1>", // <-- This will be ignored if text is present
+  "text": "Message Body" 
+}
+```
+
+- Curl:
 ```
 curl  -X POST \
-  '<<CF_WORKER_URL_HERE>>' \
+  <<CF_WORKER_URL_HERE>> \
   --header 'Accept: */*' \
-  --header 'Authorization: Bearer <<TOKEN_HERE>>' \
+  --header 'Authorization: Bearer <<EMAIL_TOKEN>>' \
   --header 'Content-Type: application/json' \
   --data-raw '{
   "to": [{
       "name": "To User Name",
-      "email": "to@user.com"
+      "email": "to@chrisbrocklesby.com"
   }],
   "from": {
     "name": "From User Name",
-    "email": "from@user.com"
+    "email": "from@chrisbrocklesby.com"
   },
   "subject": "Email Subject",
-  "html": "<h1>HTML</h1>" // <-- This will be ignored if text is present
   "text": "Message Body" 
 }'
 ```
--- main.js
+++ main.js
@@ -1,59 +1,72 @@
 export default {
   async fetch(request, env) {
-    if (request.method === "POST") {
-      const token = request.headers.get('authorization').replace("Bearer ", "");
+    if (request.method === 'POST') {
+      const token = request.headers.get('authorization').replace('Bearer ', '')
 
       if (!env.EMAIL_TOKEN || env.EMAIL_TOKEN.length === 0) {
         return new Response('You must set the Authorization Bearer Token.', {
-          status: 401,
-        });
+          status: 401
+        })
       }
 
       if (token !== env.EMAIL_TOKEN) {
-        return new Response('Unauthorized', { status: 401 });
+        return new Response('Unauthorized', { status: 401 })
       }
-      const data = await request.text();
-      const body = (!data || data.length === 0) ? {} : JSON.parse(data);
+      const data = await request.text()
+      const body = (!data || data.length === 0) ? {} : JSON.parse(data)
 
-      const response = await fetch("https://api.mailchannels.net/tx/v1/send", {
-        "method": "POST",
-        "headers": { "content-type": "application/json" },
-        "body": JSON.stringify({
-          "personalizations": [{
-            "to": body.to || [],
+      const response = await fetch('https://api.mailchannels.net/tx/v1/send', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({
+          personalizations: [{
+            to: body.to || []
           }],
-          "from": {
-            "name": body.from.name || body.from.email,
-            "email": body.from.email || " "
+          from: {
+            name: body.from.name || body.from.email,
+            email: body.from.email || ' '
           },
-          "subject": body.subject || " ",
-          "content": (body.text) ? [{
-            "type": "text/plain",
-            "value": body.text || " ",
-          }] : [{
-            "type": "text/html",
-            "value": body.html || " ",
-          }],
-        }),
+          subject: body.subject || ' ',
+          content: (body.text)
+            ? [{
+              type: 'text/plain',
+              value: body.text || ' '
+            }]
+            : [{
+              type: 'text/html',
+              value: body.html || ' '
+            }]
+        })
       })
+
+      // Debugging Logs
+      console.log(
+        JSON.stringify({
+          status: response.status,
+          message: response.statusText,
+          response: await response.json()
+        })
+      )
 
       return new Response(
         JSON.stringify({
           status: response.status,
-          message: response.statusText
+          message: response.statusText,
+          response: await response.json()
         }), {
-        status: response.status || 400,
+        status: response.status || 400
       }
-      );
+      )
     } else {
       return new Response(
         JSON.stringify({
           status: 404,
-          message: "POST request only"
+          message: 'POST request only'
         }), {
-        status: 404,
+        status: 404
       }
-      );
+      )
     }
   }
 }
+
